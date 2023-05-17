d5e4a82b1f0b69480dafe49c42bd380c7bd1623a
MoEndal <rautiainen.joona@gmail.com>
2023-05-17 16:30:15 +0300

    Deleted Harjoitus2.py and change this name to english

--- command_line_argument.py
+++ command_line_argument.py
@@ -8,33 +8,3 @@ args = parser.parse_args()
 print(sum(args.integers))
 print(min(args.integers))
 print(max(args.integers))
-
-
-
-
-
-
-#import sys
-#n = (sys.argv)
-#print(sum(len(n[1] + n[2])))
-#print(min(n[1]+n[2]))
-#print(max(n[1]+n[2]))
-
-
-
-#import argparse
-#parser = argparse.ArgumentParser()
-#parser.add_argument("luku1", help="syota luku 1", type=int)
-#parser.add_argument("luku2", help="syota luku 2", type=int)
-
-#args = parser.parse_args()
-#print(f'Yhteenlaskettu {args.luku1 + args.luku2}')
-#print(f'Pienin summa {len(args.luku1 , args.luku2)}')
-
-#import argparse
-#parser = argparse.ArgumentParser()
-#parser.add_argument("luku", type=int, nargs=3)
-#args = parser.parse_args()
-#sum = sum(args.values)
-#print("Sum: ", sum)
-
